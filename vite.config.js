d541cfb5871cc396bdc8337e1844abda6441f5ad
Sunnatillo-Dev <ruzmetovsunnatillo20081117@gmail.com>
2023-11-11 02:01:26 +0500

    added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import reactRefresh from "@vitejs/plugin-react";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRefresh()],
   optimizeDeps: {
     exclude: ["axios"],
   },
-  plugins: [reactRefresh()],
   build: {
     rollupOptions: {
       external: ["axios"],
