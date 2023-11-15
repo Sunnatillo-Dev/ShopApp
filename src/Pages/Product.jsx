2636da8dcae969cfbddf05b8eecb2cf5c57191c7
Sunnatillo-Dev <ruzmetovsunnatillo20081117@gmail.com>
2023-11-15 20:54:35 +0500

    added

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import axios from "https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm";
 import {
   Box,
   Button,
